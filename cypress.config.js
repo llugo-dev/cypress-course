b0d98ffa7594d09286f9a44487d24839d8a7a28f
Luis Lugo <dragonlugo@gmail.com>
2023-11-08 12:33:53 -0600

    First test

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 10000,
+  projectId: "q9w3b8",
+  env: {
+    url: 'https://rahulshettyacademy.com',
+    CYPRESS_RECORD_KEY: 'a51bdf97-d030-4e9c-afe2-f89563f56aba'
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -9,9 +15,5 @@ module.exports = defineConfig({
       'cypress/integration/cheqrboard/tests/*.js',
       'cypress/integration/examples/*.js'
     ]
-  },
-  defaultCommandTimeout: 10000,
-  env: {
-
   }
 });
--- cypress/integration/examples/Test10Framework.js
+++ cypress/integration/examples/Test10Framework.js
@@ -15,8 +15,8 @@ describe('Use of hooks', () => {
     it('Testing a framework', function() {
         const homepage = new HomePage();
         const productspage = new ProductsPage();
-
-        cy.visit('https://rahulshettyacademy.com/angularpractice/');
+        
+        cy.visit(Cypress.env('url') + '/angularpractice/');
         // cy.get('form div.form-group:nth-child(1) input').type(this.data.name);
         // cy.get('form div.form-group:nth-child(2) input').type(this.data.email);
         // cy.get('#exampleFormControlSelect1').select(this.data.gender);
@@ -39,15 +39,31 @@ describe('Use of hooks', () => {
 
         productspage.getProducts(this.data.productName);
         productspage.checkoutProducts().click();
-        cy.get(':nth-child(5) > :nth-child(5) > .btn').click();
-        cy.get('#country').type('United');
-        cy.get('.suggestions > :nth-child(2) > li > a').click();
-        cy.get('#checkbox2').check({force: true});
-        cy.get('input[value="Purchase"]').click();
-        // cy.get('.alert').should('contain.text', 'Success! Thank you! Your order will be delivered in next few weeks :-).');
-        cy.get('.alert').then(function (element) {
-            const text = element.text();
-            expect(text.includes('Success')).to.be.true;
+        let sum = 0;
+        cy.get('tr td:nth-of-type(4) strong').each(($element, index) => {
+            const text = $element.text();
+            const result = text.split(' ');
+            cy.log(result[1].trim());
+            sum = Number(sum) + Number(result[1].trim());
+        }).then(function (res) {
+            cy.log(sum);
         });
+
+        cy.get('td.text-right strong').then(function ($el) { 
+            const text = $el.text();
+            const amount = text.split(' ')[1];
+            expect(Number(amount)).to.equal(sum);
+        });;
+
+        // cy.get(':nth-child(5) > :nth-child(5) > .btn').click();
+        // cy.get('#country').type('United');
+        // cy.get('.suggestions > :nth-child(2) > li > a').click();
+        // cy.get('#checkbox2').check({force: true});
+        // cy.get('input[value="Purchase"]').click();
+        // // cy.get('.alert').should('contain.text', 'Success! Thank you! Your order will be delivered in next few weeks :-).');
+        // cy.get('.alert').then(function (element) {
+        //     const text = element.text();
+        //     expect(text.includes('Success')).to.be.true;
+        // });
     });
 });
